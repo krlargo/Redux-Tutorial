ca7255e8c994df5bd0eea4186fc7e406674d5a0a
krlargo <krlargo@ucdavis.edu>
2017-12-22 01:57:00 -0800

    Add state handling for BookDetail; fix import error

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class BookDetail extends Component {
 
   render() {
+    
+    if(!this.props.book) {
+      return <div>Select a book to get started.</div>;
+    }
+
     return (
       <div>
-        BookDetail
+        <h3>Details for:</h3>
+        <div>{this.props.book.title}</div>
       </div>
     )
   }
 }
 
-export default BookDetail;
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  }
+}
+
+export default connect(mapStateToProps)(BookDetail);
