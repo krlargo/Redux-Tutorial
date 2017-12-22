ff100e09637a2dfd8743a379de91666a0f6af468
krlargo <krlargo@ucdavis.edu>
2017-12-22 01:40:58 -0800

    Start BookDetail component

---
+++ src/containers/book-detail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BookDetail extends Component {
+
+  render() {
+    return (
+      <div>
+        BookDetail
+      </div>
+    )
+  }
+}
+
+export default BookDetail;
