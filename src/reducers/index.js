f22ab15118076c318f233135aea30f220de6aedd
krlargo <krlargo@ucdavis.edu>
2017-12-22 14:51:01 -0800

    Add comments

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 import ActiveBookReducer from './reducer_active_book';
 
+// Each key represents a state variable
+// - This application only has two state variables: books, activeBook
 const rootReducer = combineReducers({
   books: BooksReducer,
   activeBook: ActiveBookReducer
